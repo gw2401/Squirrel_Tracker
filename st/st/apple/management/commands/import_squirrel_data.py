f13dd1598b9ffc6a74f6780b2aa96f74ac44634f
yutongzhang1234 <yz3675@instance-2.c.vm-tools.internal>
2019-11-30 22:11:50 +0000

    second

--- st/st/apple/management/commands/import_squirrel_data.py
+++ st/st/apple/management/commands/import_squirrel_data.py
@@ -2,6 +2,7 @@ import os
 import csv
 
 from django.core.management.base import BaseCommand, CommandError
+from datetime import datetime
 
 class Command(BaseCommand):
      help = "Import students by homeroom."
@@ -12,36 +13,38 @@ class Command(BaseCommand):
      def handle(self,path,**options):
          import csv
          from apple.models import Squirrel
+         to_bool = lambda x: True if x == 'TRUE' else False
 #         import os
 #	     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 #             my_file = os.path.join(THIS_FOLDER, 'myfile.txt')
 #	 path = str(path)
          with open(path, 'r') as f:
             reader = csv.reader(f)
+            next(reader, None)
             for row in reader:
                  squirrel = Squirrel.objects.get_or_create(
-                            Latitude = row[0],
-                            Longitude = row[1],
+                            Latitude = float(row[0]),
+                            Longitude = float(row[1]),
                             Unique_Squirrel_ID = row[2],
                             Shift = row[4],
-                            Date = row[5],
+                            Date = datetime.strptime(row[5],'%m%d%Y'),
                             Age = row[7],
                             Primary_Fur_Color = row[8],
                             Location = row[12],
                             Specific_Location = row[14],
-                            Running = row[15],
-                            Chasing = row[16],
-                            Climbing = row[17],
-                            Eating = row[18],
-                            Foraging = row[19],
-                            Other_Activities = row[20],
-                            Kuks = row[21],
-                            Quaas = row[22],
-                            Moans = row[23],
-                            Tail_flags = row[24],
-                            Tail_twitches = row[25],
-                            Approaches = row[26],
-                            Indifferent = row[28],
-                            Runs_from = row[28],
+                            Running = to_bool(row[15]),
+                            Chasing = to_bool(row[16]),
+                            Climbing = to_bool(row[17]),
+                            Eating = to_bool(row[18]),
+                            Foraging = to_bool(row[19]),
+                            Other_Activities = to_bool(row[20]),
+                            Kuks = to_bool(row[21]),
+                            Quaas = to_bool(row[22]),
+                            Moans = to_bool(row[23]),
+                            Tail_flags = to_bool(row[24]),
+                            Tail_twitches = to_bool(row[25]),
+                            Approaches = to_bool(row[26]),
+                            Indifferent = to_bool(row[28]),
+                            Runs_from = to_bool(row[28]),
                             )
 
